8eb6a9ab81247e16b80ebf3a67b7fe1ce0eeb998
Bezara06 <increasescientific@gmail.com>
2026-01-15 12:15:39 +0300

    Enhance navigation and styling in HTML and CSS; update dynamic text in JavaScript

--- dom.js
+++ dom.js
@@ -9,8 +9,10 @@ function hide() {
         document.querySelector('h1').style.display = 'block';
     }
 }
+let tmp = 1
 function affiche() {
     const elem = document.createElement('p');
-    elem.textContent = "Hello , world !";
+    elem.textContent = "Hello , world !" + tmp;
     document.body.appendChild(elem);
+    tmp++
 }
